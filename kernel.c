83a050411b8d327b8135790e695942d4e6980595
Swapna764 <swapnamudavath231@gmail.com>
2026-04-07 19:02:36 +0000

    Making changes

--- kernel.c
+++ kernel.c
@@ -1,17 +1,32 @@
-void main()
-{
-    volatile char* video = (volatile char*) 0xb8000;
+void main() {
+    char *video = (char *)0xb8000;
+    int i;
 
-    char msg[] = "Math OS Kernel Result: 5 + 3 = 8";
-
-    int i = 0;
-
-    while(msg[i] != '\0')
-    {
-        video[i*2] = msg[i];
-        video[i*2 + 1] = 0x07;
-        i++;
+    // Clear screen (first 80*25*2 bytes)
+    for (i = 0; i < 80*25*2; i++) {
+        video[i] = (i == 1) ? ' ' : 0x00;  // Space char, black bg
     }
 
-    while(1);
+    // Print "Kernel OK" at row 0
+    char *msg = "Kernel OK";
+    for (i = 0; msg[i] != '\0'; i++) {
+        video[(0*80 + i)*2] = msg[i];
+        video[(0*80 + i)*2 + 1] = 0x07;  // Light grey on black
+    }
+
+    // Demo math calc: Simple pi approximation (Leibniz formula, 100k terms)
+    double pi = 0.0;
+    for (i = 0; i < 100000; i++) {
+        pi += (i % 2 ? -1.0 : 1.0) / (2*i + 1);
+    }
+    pi *= 4;
+
+    // Print pi approx at row 2 (rough ASCII, since no float print)
+    char math_msg[] = "Pi approx: 3.14... (see calc)";
+    for (i = 0; math_msg[i] != '\0'; i++) {
+        video[(2*80 + i)*2] = math_msg[i];
+        video[(2*80 + i)*2 + 1] = 0x07;
+    }
+
+    while (1);  // Halt
 }
