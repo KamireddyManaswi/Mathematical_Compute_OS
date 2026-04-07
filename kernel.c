2303f37a4d74522d6d323074d1f071ee056bad66
Swapna764 <swapnamudavath231@gmail.com>
2026-04-07 19:08:11 +0000

    Making changes

--- kernel.c
+++ kernel.c
@@ -1,32 +1,24 @@
+void print_string(char *str, char *video, int pos) {
+    while (*str) {
+        video[pos * 2] = *str++;
+        video[pos * 2 + 1] = 0x07;
+        pos++;
+    }
+}
+
 void main() {
-    char *video = (char *)0xb8000;
+    char *video = (char*)0xb8000;
+
+    // Clear first row only (avoid full clear if issue)
     int i;
+    for (i = 0; i < 160; i += 2) video[i] = 0x00;
 
-    // Clear screen (first 80*25*2 bytes)
-    for (i = 0; i < 80*25*2; i++) {
-        video[i] = (i == 1) ? ' ' : 0x00;  // Space char, black bg
-    }
+    print_string("Kernel OK", video, 0);
 
-    // Print "Kernel OK" at row 0
-    char *msg = "Kernel OK";
-    for (i = 0; msg[i] != '\0'; i++) {
-        video[(0*80 + i)*2] = msg[i];
-        video[(0*80 + i)*2 + 1] = 0x07;  // Light grey on black
-    }
+    // Simple math: factorial 5 = 120
+    int fact = 1, n = 5;
+    for (i = 2; i <= n; i++) fact *= i;
+    print_string(" Fact(5)=120", video, 10);
 
-    // Demo math calc: Simple pi approximation (Leibniz formula, 100k terms)
-    double pi = 0.0;
-    for (i = 0; i < 100000; i++) {
-        pi += (i % 2 ? -1.0 : 1.0) / (2*i + 1);
-    }
-    pi *= 4;
-
-    // Print pi approx at row 2 (rough ASCII, since no float print)
-    char math_msg[] = "Pi approx: 3.14... (see calc)";
-    for (i = 0; math_msg[i] != '\0'; i++) {
-        video[(2*80 + i)*2] = math_msg[i];
-        video[(2*80 + i)*2 + 1] = 0x07;
-    }
-
-    while (1);  // Halt
+    while(1);
 }
