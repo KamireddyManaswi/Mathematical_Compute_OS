92110fe9191aa036e05c4bf70862d9ff1c3437b7
Kamireddymanaswi <manaswikamireddy434@gmail.com>
2026-04-07 18:04:52 +0000

    updated boot.asm

--- boot.asm
+++ boot.asm
@@ -2,26 +2,21 @@
 [ORG 0x7C00]
 
 start:
-    ; Print 'B' to show bootloader is running
     mov ah, 0x0E
     mov al, 'B'
     int 0x10
 
-    ; Load kernel from disk (starting at sector 2)
-    mov ah, 0x02      ; BIOS read function
-    mov al, 5         ; number of sectors to read
-    mov ch, 0         ; cylinder
-    mov cl, 2         ; sector (boot = 1, kernel starts at 2)
-    mov dh, 0         ; head
-    mov dl, 0x00      ; drive (floppy)
-    mov bx, 0x1000    ; load address
-    int 0x13          ; BIOS interrupt
+    ; Load kernel from disk
+    mov ah, 0x02
+    mov al, 5
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
+    mov dl, 0x00
+    mov bx, 0x1000
+    int 0x13
 
-    ; Jump to kernel
     jmp 0x0000:0x1000
 
-; Fill remaining bytes to make 512 bytes
 times 510-($-$$) db 0
-
-; Boot signature (VERY IMPORTANT)
 dw 0xAA55
