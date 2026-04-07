a3529a8bc98a67e63723fb69233ee74b0d400ab4
Kamireddymanaswi <manaswikamireddy434@gmail.com>
2026-04-07 19:49:22 +0000

    updated boot.asm

--- boot.asm
+++ boot.asm
@@ -2,50 +2,23 @@
 [ORG 0x7C00]
 
 start:
-    cli
+    ; print B (bootloader working)
+    mov ah, 0x0E
+    mov al, 'B'
+    int 0x10
 
-    xor ax, ax
-    mov ds, ax
-    mov es, ax
-    mov ss, ax
-    mov sp, 0x7C00
-
-    mov si, boot_msg
-    call print_str
-
-
-; load kernel from disk (sector 2 onwards) to memory 0x1000
-
+    ; load kernel (ONLY 1 sector)
     mov ah, 0x02
-    mov al, 10        ; number of sectors to read
+    mov al, 1
     mov ch, 0
-    mov cl, 2         ; start reading from sector 2
+    mov cl, 2
     mov dh, 0
     mov dl, 0x00
-    mov bx, 0x1000    ; memory location for kernel
+    mov bx, 0x1000
     int 0x13
 
-
-; jump to kernel
-
+    ; jump to kernel
     jmp 0x0000:0x1000
-
-
-
-print_str:
-    mov ah, 0x0E
-.next:
-    lodsb
-    cmp al, 0
-    je .done
-    int 0x10
-    jmp .next
-.done:
-    ret
-
-
-boot_msg db "Booting kernel...",13,10,0
-
 
 times 510-($-$$) db 0
 dw 0xAA55
