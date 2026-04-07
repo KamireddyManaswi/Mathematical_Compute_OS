9eae1b66e1aff3e9020ce53542993926e66811d4
Swapna764 <swapnamudavath231@gmail.com>
2026-04-07 19:42:11 +0000

    Making changes

--- kernel.c
+++ kernel.c
@@ -1,24 +1,14 @@
-void print_string(char *str, char *video, int pos) {
-    while (*str) {
-        video[pos * 2] = *str++;
-        video[pos * 2 + 1] = 0x07;
-        pos++;
-    }
-}
-
 void main() {
-    char *video = (char*)0xb8000;
+    char *video = (char*) 0xb8000;
 
-    // Clear first row only (avoid full clear if issue)
-    int i;
-    for (i = 0; i < 160; i += 2) video[i] = 0x00;
+    char msg[] = "Kernel: Math OS Ready | 5 + 3 = 8";
 
-    print_string("Kernel OK", video, 0);
-
-    // Simple math: factorial 5 = 120
-    int fact = 1, n = 5;
-    for (i = 2; i <= n; i++) fact *= i;
-    print_string(" Fact(5)=120", video, 10);
+    int i = 0;
+    while (msg[i] != '\0') {
+        video[i * 2] = msg[i];
+        video[i * 2 + 1] = 0x07;
+        i++;
+    }
 
     while(1);
 }
