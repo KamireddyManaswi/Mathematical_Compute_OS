b79e2befc37db3fc1de24696d6f3b1c0ba3a38e4
Kamireddymanaswi <manaswikamireddy434@gmail.com>
2026-04-07 18:55:08 +0000

    updated boot.asm

--- boot.asm
+++ boot.asm
@@ -2,21 +2,25 @@
 [ORG 0x7C00]
 
 start:
-    mov ah, 0x0E
-    mov al, 'B'
-    int 0x10
 
-    ; load kernel (sector 2 → memory 0x1000)
-    mov ah, 0x02
-    mov al, 1        ; load 1 sector (very important)
-    mov ch, 0
-    mov cl, 2
-    mov dh, 0
-    mov dl, 0x00
-    mov bx, 0x1000
-    int 0x13
+    ; BIOS loads bootloader at 0x7C00
+    ; load kernel (next sectors) into memory 0x1000
 
+    mov ah, 0x02      ; BIOS read sector function
+    mov al, 20        ; number of sectors to read
+    mov ch, 0         ; cylinder
+    mov cl, 2         ; sector number (start after boot sector)
+    mov dh, 0         ; head
+    mov dl, 0x00      ; drive (floppy in qemu)
+    mov bx, 0x1000    ; memory location
+    int 0x13          ; call BIOS
+
+    ; jump to kernel
     jmp 0x0000:0x1000
 
+
+; fill remaining bytes with 0
 times 510-($-$$) db 0
+
+; boot signature
 dw 0xAA55
