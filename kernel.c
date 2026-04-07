bd27cbd3006a0eecf8dc6117a3d95c23ce0381cc
Swapna764 <swapnamudavath231@gmail.com>
2026-04-07 19:46:32 +0000

    Making changes

--- kernel.c
+++ kernel.c
@@ -1,14 +1,46 @@
-void main() {
+void print_string(char *str, char *video, int pos)
+{
+    while(*str)
+    {
+        video[pos*2] = *str;
+        video[pos*2+1] = 0x07;
+
+        str++;
+        pos++;
+    }
+}
+
+
+void main()
+{
     char *video = (char*) 0xb8000;
 
-    char msg[] = "Kernel: Math OS Ready | 5 + 3 = 8";
+    print_string("Kernel OK", video, 0);
 
-    int i = 0;
-    while (msg[i] != '\0') {
-        video[i * 2] = msg[i];
-        video[i * 2 + 1] = 0x07;
-        i++;
-    }
+
+    int fact = 1;
+    int n = 5;
+
+    int i;
+
+    for(i=2;i<=n;i++)
+        fact *= i;
+
+
+    print_string(" Fact(5)=", video, 12);
+
+
+    char result = fact + '0';   // 120 simplified demo
+
+    video[26] = '1';
+    video[27] = 0x07;
+
+    video[28] = '2';
+    video[29] = 0x07;
+
+    video[30] = '0';
+    video[31] = 0x07;
+
 
     while(1);
 }
