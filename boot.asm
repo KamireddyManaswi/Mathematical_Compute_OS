3611b46f3e4ef4f75e691b5d9f0a6e6c1d29d0d1
Kamireddymanaswi <manaswikamireddy434@gmail.com>
2026-04-07 19:41:14 +0000

    updated boot.asm

--- boot.asm
+++ boot.asm
@@ -2,77 +2,23 @@
 [ORG 0x7C00]
 
 start:
-    xor ax, ax
-    mov ds, ax
-    mov es, ax
-
-    mov si, boot_msg
-    call print_str
-
-    mov si, kern_msg
-    call print_str
-
-    mov si, fact_msg
-    call print_str
-
-    ; -------- factorial 6 = 720 --------
-    mov ax, 1
-    mov cx, 6
-
-fact_loop:
-    mul cx
-    loop fact_loop
-
-    ; now AX = 720
-
-    call print_number
-
-    mov si, done_msg
-    call print_str
-
-hang:
-    jmp hang
-
-; ---------------- PRINT NUMBER ----------------
-print_number:
-    mov bx, 10
-    xor cx, cx
-
-convert:
-    xor dx, dx
-    div bx
-    push dx
-    inc cx
-    cmp ax, 0
-    jne convert
-
-print_digits:
-    pop dx
-    add dl, '0'
+    ; print B (bootloader running)
     mov ah, 0x0E
-    mov al, dl
+    mov al, 'B'
     int 0x10
-    loop print_digits
 
-    ret
+    ; load kernel (1 sector is enough)
+    mov ah, 0x02
+    mov al, 1        ; load 1 sector
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
+    mov dl, 0x00
+    mov bx, 0x1000
+    int 0x13
 
-; ---------------- PRINT STRING ----------------
-print_str:
-    mov ah, 0x0E
-.next:
-    lodsb
-    cmp al, 0
-    je .done
-    int 0x10
-    jmp .next
-.done:
-    ret
-
-; ---------------- DATA ----------------
-boot_msg db 'Booting kernel...',13,10,0
-kern_msg db 'Kernel OK',13,10,0
-fact_msg db 'Fact(6)=',0
-done_msg db ' Math OS ready!',13,10,0
+    ; jump to kernel
+    jmp 0x0000:0x1000
 
 times 510-($-$$) db 0
 dw 0xAA55
