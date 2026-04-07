0cf993c68c38a1b3e761db15fb26ed7ae05aebf0
Kamireddymanaswi <manaswikamireddy434@gmail.com>
2026-04-07 19:16:06 +0000

    updated boot.asm

--- boot.asm
+++ boot.asm
@@ -5,40 +5,68 @@ start:
     xor ax, ax
     mov ds, ax
     mov es, ax
+    mov ss, ax
+    mov sp, 0x7C00        ; Stack below boot sector
 
-    mov si, msg_boot
-    call print_string
+    mov si, boot_msg
+    call print_str
 
-    ; Load kernel sector 2 to 0x1000
-    mov ah, 0x02
-    mov al, 1
-    mov ch, 0
-    mov cl, 2
-    mov dh, 0
-    mov bx, 0x1000  ; ES:BX
-    int 0x13
-    jc error
+    jmp kernel             ; "Load" = jump to kernel code (fits in boot sector)
 
-    jmp 0x1000
+kernel:
+    mov si, kern_msg
+    call print_str
 
-error:
-    mov si, msg_err
-    call print_string
-    hlt
+    ; Math demo: Compute factorial 6 = 720
+    mov ax, 1              ; fact = 1
+    mov cx, 6              ; n = 6
+fact_loop:
+    mul cx                 ; ax *= cx
+    loop fact_loop         ; cx--, loop if cx > 0
 
-print_string:
+    ; Print "720 " (digits in ax)
+    mov bx, ax
+    mov ax, '0'
+    mov cx, 3
+print_digits:
+    mov si, digit_buf + 2  ; 3-digit buffer
+    sub si, cx
+    mov [si], al
+    dec cx
+    jz print_fact
+    mov dx, 10
+    xor dx, dx             ; Divide bx / 10
+    mov ax, bx
+    div word [ten]
+    mov bx, dx             ; Remainder to bx
+    mov ax, '0'
+    add al, bl
+    jmp print_digits
+print_fact:
+    mov si, digit_buf
+    call print_str
+
+    mov si, done_msg
+    call print_str
+
+hang: hlt
+    jmp hang
+
+print_str:
     mov ah, 0x0E
-.loop:
-    lodsb
+.loop: lodsb
     test al, al
     jz .done
     int 0x10
     jmp .loop
-.done:
-    ret
+.done: ret
 
-msg_boot db 'Booting kernel...', 0
-msg_err  db 'Load error!', 0
+boot_msg db 'Booting kernel...', 13, 10, 0
+kern_msg db 'Kernel OK', 13, 10, 0
+fact_msg db 'Fact(6)=', 0
+done_msg db ' Math OS ready!', 13, 10, 0
+digit_buf db 0,0,0
+ten dw 10
 
 times 510-($-$$) db 0
 dw 0xAA55
