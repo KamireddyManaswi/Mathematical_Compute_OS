b02223aa51905697dad8efd4f76c713807308315
Kamireddymanaswi <manaswikamireddy434@gmail.com>
2026-04-07 19:58:35 +0000

    updated boot.asm

--- boot.asm
+++ boot.asm
@@ -2,23 +2,45 @@
 [ORG 0x7C00]
 
 start:
-    ; print B (bootloader working)
+    mov [BOOT_DRIVE], dl   ; save boot drive
+
+    ; print B
     mov ah, 0x0E
     mov al, 'B'
     int 0x10
 
     ; -------- LOAD KERNEL --------
-    mov ah, 0x02      ; BIOS read function
-    mov al, 10        ; number of sectors
+    mov ah, 0x02
+    mov al, 10
     mov ch, 0
     mov cl, 2
     mov dh, 0
-    mov dl, 0x00
+    mov dl, [BOOT_DRIVE]   ; ✅ use correct drive
     mov bx, 0x1000
     int 0x13
 
-    ; jump to kernel
+    jc disk_error          ; if error → jump
+
     jmp 0x0000:0x1000
+
+disk_error:
+    mov si, err_msg
+    call print
+    jmp $
+
+print:
+    mov ah, 0x0E
+.next:
+    lodsb
+    cmp al, 0
+    je .done
+    int 0x10
+    jmp .next
+.done:
+    ret
+
+BOOT_DRIVE db 0
+err_msg db "Disk Error!",0
 
 times 510-($-$$) db 0
 dw 0xAA55
